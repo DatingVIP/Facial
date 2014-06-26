e3676f9dcce50cb7161993db8190fe7cbd444ec6
Joe <joe.watkins@live.co.uk>
2014-06-26 12:29:13 +0100

    update test script to scan directory

--- facial.php
+++ facial.php
@@ -1,19 +1,32 @@
 <?php
-$image   = "images/sepia.jpg";
-$size    = getimagesize($image);
-$cascade = cascade("cascades/default.xml");
+/**
+* Scan a directory for images creating test images ...
+**/
 
-$copy    = imagecreatefromjpeg($image);
-$green   = imagecolorallocate($copy, 132, 135, 28);
+/** usage: facial.php [path=images] [cascade=cascades/default.xml] **/
 
-foreach (faces($cascade, $image, $size[0], $size[1]) as $face) {
-	imagerectangle($copy, 
-		$face["x"],
-		$face["y"],
-		$face["x"] + $face["width"],
-		$face["y"] + $face["height"],
-		$green);
+$path    = @realpath($argv[1] ?: "images");
+$cascade = @cascade($argv[2] ?: "cascades/default.xml");
+
+foreach (glob("{$path}/*.jpg") as $image) {
+	if (preg_match("~/face-~", $image)) {
+		continue;	
+	}
+	
+	$base    = basename($image);
+	$size    = getimagesize($image);
+	$copy    = imagecreatefromjpeg($image);
+	$green   = imagecolorallocate($copy, 132, 135, 28);
+
+	foreach (faces($cascade, $image, $size[0], $size[1]) as $face) {
+		imagerectangle($copy, 
+			$face["x"],
+			$face["y"],
+			$face["x"] + $face["width"],
+			$face["y"] + $face["height"],
+			$green);
+	}
+
+	imagejpeg($copy, "{$path}/face-{$base}");
 }
-
-imagejpeg($copy, "images/sepia-face.jpg");
 ?>
