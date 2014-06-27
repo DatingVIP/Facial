9487c2b975bcd22b6a417599182d78c6db6d2b48
Joe <joe.watkins@live.co.uk>
2014-06-27 13:12:57 +0100

    ...

--- facial.php
+++ facial.php
@@ -7,8 +7,25 @@
 
 $path    = @realpath($argv[1] ?: "images");
 $cascade = @cascade($argv[2]  ?: "cascades/default.xml");
+$draw    = (bool) @$argv[3];
 
-foreach (glob("{$path}/*.jpg") as $image) {
+function find($path) {
+	$files = [];
+	foreach (scandir($path) as $file) {
+		if ($file == "." || $file == "..") {
+			continue;
+		}
+		
+		$next = "{$path}/{$file}";
+		if (is_dir($next)) {
+			$files = array_merge
+				($files, find($next));
+		} else $files[] = $next;
+	}
+	return $files;
+}
+
+foreach (find(realpath($path)) as $image) {
 	if (preg_match("~/face-~", $image)) {
 		continue;	
 	}
@@ -18,13 +35,18 @@ foreach (glob("{$path}/*.jpg") as $image) {
 	
 	$faces   = faces($cascade, $image, $size[0], $size[1]);
 
-	if (!$faces) {h
+	if (!$faces) {
+		$fail++;
 		printf("[F]: no faces found in %s\n", $image);
 		continue;
-	}
+	} else $pass++;
 
 	printf("[P]: %d faces found in %s\n", count($faces), $image);
 
+	if (!$draw) {
+		continue;
+	}
+	
 	$copy    = imagecreatefromjpeg($image);
 	$green   = imagecolorallocate($copy, 132, 135, 28);
 
@@ -39,4 +61,8 @@ foreach (glob("{$path}/*.jpg") as $image) {
 
 	imagejpeg($copy, "{$path}/face-{$base}");
 }
+
+printf(
+	"%d failures, %d detections, %.3f fails\n",
+	$fail, $pass, (($fail + $pass) / 100) * $fail);
 ?>
